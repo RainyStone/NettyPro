eb13371c42c6a4b6b30735a3cd853d6f4d6bced2
xzx <3144790170@qq.com>
2021-03-10 22:59:20 +0800

    complete BIOServer.java,practice BIO demo with telnet client

--- src/main/java/com/atguigu/bio/BIOServer.java
+++ src/main/java/com/atguigu/bio/BIOServer.java
@@ -1,5 +1,7 @@
 package com.atguigu.bio;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -16,9 +18,34 @@ public class BIOServer {
             newCachedThreadPool.execute(new Runnable() {
                 @Override
                 public void run() {
-
+                    handler(socket);
                 }
             });
+        }
+    }
+
+    public static void handler(Socket socket){
+        try{
+            byte[] bytes = new byte[1024];
+            InputStream inputStream = socket.getInputStream();
+            while (true){
+                int read = inputStream.read(bytes);
+                if(read!=-1){
+                    System.out.println(new String(bytes,0,read));
+                }else {
+                    System.out.println("数据读取完毕");
+                    break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("关闭与client的连接");
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
